5d84be8e9c6bfdacffe3e7ccf5e7c556b942f5e6
Lynsey Kirk <55623711+lk119@users.noreply.github.com>
2019-11-27 15:36:52 +0000

    Create MainComp.java

---
+++ MainComp.java
@@ -0,0 +1,14 @@
+public class MainComp {
+
+	public static void main(String[] args) {
+		CompetitorList cl = new CompetitorList();
+		cl .readFile("competitorList.csv");
+		String report = cl.getAllCompetitors();
+		cl .writeToFile("competitorReport.txt", report);
+
+		CompetitorManager cm = new CompetitorManager();
+		cm.setCL(cl);
+		cm.gui();
+		
+	}
+}
