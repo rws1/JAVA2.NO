2bea0e7c3e99e7270a4d27926665ea944fe1abc4
Lynsey Kirk <55623711+lk119@users.noreply.github.com>
2019-11-27 17:28:30 +0000

    Update CompetitorManager.java

--- CompetitorManager.java
+++ CompetitorManager.java
@@ -10,16 +10,17 @@ public class CompetitorManager {
 	}
 	
 	
-	public void gui() {
-		GUIOne g1 = new GUIOne();
+	public void gui() {		
+		
+		GUIOne g1 = new GUIOne(allCompetitors);
 		g1.pack();  
 		g1.setVisible(true);
 		
-		GUITwo g2 = new GUITwo();
+		GUITwo g2 = new GUITwo(allCompetitors);
 		g2.pack();  
 		g2.setVisible(true);
 		
-		GUIThree g3 = new GUIThree();
+		GUIThree g3 = new GUIThree(allCompetitors);
 		g3.pack();  
 		g3.setVisible(true);
 	}
