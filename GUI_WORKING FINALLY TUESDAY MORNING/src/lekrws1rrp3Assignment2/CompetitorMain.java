b296680468e422f4010ab9e176c779f645107ff1
rrp3 <55551866+RachPatel@users.noreply.github.com>
2019-12-03 11:39:49 +0000

    VANITY FAIR FOR ROB ;D

---
+++ GUI_WORKING FINALLY TUESDAY MORNING/src/lekrws1rrp3Assignment2/CompetitorMain.java
@@ -0,0 +1,12 @@
+package lekrws1rrp3Assignment2;
+
+public class CompetitorMain {
+
+	public static void main(String[] arg) {
+		
+		
+		CompetitorManager Manager = new CompetitorManager();
+		Manager.run();
+	}	
+	
+	}
---
+++ GUI_WORKING FINALLY TUESDAY MORNING/src/lekrws1rrp3Assignment2/Name.java
@@ -0,0 +1,57 @@
+package lekrws1rrp3Assignment2;
+
+public class Name {
+	// Assign parameters
+
+		private String FirstName;
+		private String MiddleName;
+		private String LastName;
+
+		// Create the constructor
+
+		public Name(String fName, String mName, String lName) {
+
+			this.FirstName = fName;
+			this.MiddleName = mName;
+			this.LastName = lName;
+
+		}
+		
+		
+		
+		
+		public Name (String fullName) {
+			  int spacePos1 = fullName.indexOf(' ');
+			  FirstName = fullName.substring(0, spacePos1);
+			  int spacePos2 = fullName.lastIndexOf(' ');
+			  if (spacePos1 == spacePos2)
+				  MiddleName = "";
+			  else 
+				  MiddleName = fullName.substring(spacePos1+1, spacePos2);
+			  LastName = fullName.substring(spacePos2 + 1);  
+		}
+
+		// Create get method for full name
+
+		public String getFullName() {
+
+			return FirstName + " " + MiddleName + " " + LastName;
+
+		}
+
+		// Create get method for firstName
+
+		public String getfirstName() {
+
+			return FirstName;
+		}
+
+		// Create get method for Initials
+		public String getInititals() {
+
+			return FirstName.substring(0, 1) + MiddleName.substring(0, 1) + LastName.substring(0, 1);
+		}
+
+		
+
+}
