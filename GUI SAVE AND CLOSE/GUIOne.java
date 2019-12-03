4c5c8163558a55a4cfa4b0da6e1b4167ffbd7933
Lynsey Kirk <55623711+lk119@users.noreply.github.com>
2019-12-03 13:56:15 +0000

    Update GUIOne.java

--- GUI SAVE AND CLOSE/GUIOne.java
+++ GUI SAVE AND CLOSE/GUIOne.java
@@ -4,251 +4,305 @@ import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
 
-public class GUIOne extends JFrame implements ActionListener {
-	
-	
-	//the competitor list
+public class GUITwo extends JFrame implements ActionListener {
+
+	// the competitor list
 	private CompetitorList compList;
 
-	//GUI components
-	JTextField compNumberField, nameField, locationField, levelField, overallScore, searchField;
-	JTextField [] scoresField;
-	JLabel result;
-	JButton search, update, close;
+	// GUI components
+	JScrollPane scrollList;
+	JButton submit, orderListByID, orderListByName, close;
+	JTextArea resultList;
+	JComboBox<String> competitionList = new JComboBox<>();
+	DefaultComboBoxModel<String> compModel = new DefaultComboBoxModel<>();
 
-	 
-	 public GUIOne(CompetitorList list) {
-		 
+	public GUITwo(CompetitorList list) {
+
 		this.compList = list;
 
-		 //set up window title
-		 setTitle("Competitor Details");
-		 //disable standard close button
-		 setDefaultCloseOperation(this.DO_NOTHING_ON_CLOSE);
-		 
-		 setupSouthPanel();
-		 setupNorthPanel();
-		 setupCenterPanel();
-		 
-		//pack, set visible and locate in middle of screen
-		 pack();
-		 setVisible(true);
-		 setLocationRelativeTo(null); 
-		 
-		 //show JOptionpane confirmation when close button pressed
-		 addWindowListener(new WindowAdapter() {
-			 public void windowClosing(WindowEvent e) {
-			   int confirmed = JOptionPane.showConfirmDialog(null, 
-			        "Are you sure you want to exit? You're changes will not be saved.", "Exit Programme", JOptionPane.YES_NO_OPTION);
+		// set up window title
+		setTitle("Competitor Details");
 
-			   if (confirmed == JOptionPane.YES_OPTION) {
-			      dispose();
-			   }
-			 }
+		// disable standard close button
+		setDefaultCloseOperation(this.DO_NOTHING_ON_CLOSE);
+
+		setupSouthPanel();
+		setupNorthPanel();
+		setupCenterPanel();
+
+		// pack, set visible and locate in middle of screen
+		pack();
+		setVisible(true);
+		setLocationRelativeTo(null);
+
+		// show JOptionpane confirmation when close button pressed
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				int confirmed = JOptionPane.showConfirmDialog(null,
+						"Are you sure you want to exit? You're changes will not be saved.", "Exit Programme",
+						JOptionPane.YES_NO_OPTION);
+
+				if (confirmed == JOptionPane.YES_OPTION) {
+					dispose();
+				}
+			}
 		});
-	 }
-	 	
-	 private void setupNorthPanel() {
-		//search panel contains label, text field and button
-		JPanel searchPanel = new JPanel();
-		searchPanel.setLayout(new GridLayout(2,2));
-		
-		//setup search field
-		searchField = new JTextField(5);
-		
-		//setup label
-		result = new JLabel("");
-		result.setForeground(Color.RED);
-		
-		//setup search button
-		search = new JButton("Search");
+	}
 
-		//add components to search panel
-		searchPanel.add(new JLabel("  Competitor Number:  "));
-		searchPanel.add(searchField);
-		searchPanel.add(result);
-		searchPanel.add(search);
-		
-		//add the event listener to the buttons
-		search.addActionListener(this);
-		 
-		//add search panel to border layout
-		this.add(searchPanel, BorderLayout.NORTH);
-		}
-	 
-	 private void setupCenterPanel() {
-		//set up centre panel with labels, text fields, buttons and panels
-		JPanel centerPanel = new JPanel();
-		centerPanel.setLayout(new GridLayout(6,2));
-		
-		//setup competitor number field
-		JLabel compNumberLabel = new JLabel("  Competitor Number:  ");
-		compNumberField= new JTextField(5);
-		compNumberField.setEditable(false);
-		
-		//setup competitor name field
-		JLabel nameLabel = new JLabel("  Name:");
-		nameField= new JTextField(10);
-		nameField.setEditable(false);
-		
-		//setup location name field
-		JLabel locationLabel = new JLabel("  Location:");
-		locationField = new JTextField(5);
-		locationField.setEditable(false);
-		
-		//setup level name field
-		JLabel levelLabel = new JLabel("  Experience:");
-		levelField = new JTextField(5);
-		levelField.setEditable(false);
-		
-		//setup panel for scores
-		JLabel scoresLabel = new JLabel("  Scores:");
-		JPanel scoresPanel = new JPanel();
-		//assumes only 5 scores
-		scoresField = new JTextField[5];
-		for (int i = 0; i < 5; i++) {
-			  scoresField[i] = new JTextField(2);
-			  scoresField[i].setEditable(true);
-			  //add to scores panel
-			  scoresPanel.add((scoresField[i]));
-		  }
-		  
-		//setup dummy label
-		JLabel dummy = new JLabel("");
-		
-		//setup update button
-		update = new JButton ("Update");
-		update.setEnabled(false);
-		
-		//add components to centre panel
-		centerPanel.add(compNumberLabel);
-		centerPanel.add(compNumberField);
-		centerPanel.add(nameLabel);
-		centerPanel.add(nameField);
-		centerPanel.add(locationLabel);
-		centerPanel.add(locationField);
-		centerPanel.add(levelLabel);
-		centerPanel.add(levelField);
-		centerPanel.add(scoresLabel);
-		centerPanel.add(scoresPanel);
-		centerPanel.add(dummy);
-		centerPanel.add(update);
-		  
-		//add the event listener to the buttons
-		update.addActionListener(this);
+	private void setupNorthPanel() {
+		// north panel contains label, combo box and button
+		JPanel northPanel = new JPanel();
+		northPanel.setLayout(new GridLayout(1, 3));
 
-		//add centre panel to the content pane
-		this.add(centerPanel, BorderLayout.CENTER);
-		}
-	 
-	 
-	 private void setupSouthPanel() {
-		//setup south panel with label and text field containing overall score
+		// setup label
+		northPanel.add(new JLabel(" Select Compitition:  "));
+
+		// setup combo box
+		compModel.addElement("All competitors");
+		compModel.addElement("Dancing");
+		compModel.addElement("Gaming");
+		compModel.addElement("Knitting");
+
+		competitionList.setModel(compModel);
+		competitionList.setEditable(false);
+
+		// setup submit button
+		submit = new JButton("Submit");
+
+		// add components to north panel
+		northPanel.add(competitionList);
+		northPanel.add(submit);
+
+		// add the event listener to the buttons
+		submit.addActionListener(this);
+
+		// add north panel to the content pane
+		this.add(northPanel, BorderLayout.NORTH);
+	}
+
+	private void setupCenterPanel() {
+		// centre panel contains text area for results to appear
+		resultList = new JTextArea(15, 20);
+		resultList.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 14));
+		resultList.setEditable(false);
+		scrollList = new JScrollPane(resultList);
+
+		// add scroll list to border layout
+		this.add(scrollList, BorderLayout.CENTER);
+
+	}
+
+	private void setupSouthPanel() {
+		// south panel contains sort buttons and close button
 		JPanel southPanel = new JPanel();
-		southPanel.setLayout(new GridLayout(3,1));
-		
-		//setup overall score field
-		overallScore = new JTextField(25);
-		overallScore.setEditable(false);
-		
-		//setup close button
-		close = new JButton("Save and close");
-		 
-		//add components to south panel
-		southPanel.add(new JLabel("  Overall Score: "));
-		southPanel.add(overallScore);
+		orderListByID = new JButton("Order By ID");
+		orderListByName = new JButton("Order By Name");
+		close = new JButton("Close");
+
+		// add components to south panel
+		southPanel.add(orderListByID);
+		southPanel.add(orderListByName);
 		southPanel.add(close);
-		
-		 //add the event listener to the buttons
-		 close.addActionListener(this);
-		 
-		 
-		//add south panel to the content pane
-		 this.add(southPanel, BorderLayout.SOUTH);
-	 }
-	 
-	//setup the actions to be performed
+
+		// add the event listener to the buttons
+		orderListByID.addActionListener(this);
+		orderListByName.addActionListener(this);
+		close.addActionListener(this);
+
+		// add south panel to the border layout
+		this.add(southPanel, BorderLayout.SOUTH);
+	}
+
+	// setup the actions to be performed
 	public void actionPerformed(ActionEvent e) {
-		//if submit button then use search method
-		if (e.getSource() == search) {
-			  search();
+
+		// if submit button then use search method
+		if (e.getSource() == submit) {
+			search();
 		}
-		//if update button then use update method
-		else if (e.getSource() == update) {
-		  update();
+		// if order by ID button then use sortByID method
+		else if (e.getSource() == orderListByID) {
+			sortByID();
 		}
-		//if update button then use update method
-		else if (e.getSource() == close) {
-		  String report = compList.getAllCompetitors();
-		  compList.writeToFile("CompetitorReport.txt", report);
-		  dispose();
+		// if order by name button then use sortByName method
+		else if (e.getSource() == orderListByName) {
+			sortByName();
+		} else if (e.getSource() == close) {
+			String report = compList.getAllCompetitors();
+			compList.writeToFile("CompetitorReport.txt", report);
+			dispose();
 		}
-	 }
-	 
-	 private void search() {
-		 //get input and trim to remove additional spaces
-		 String searchString = searchField.getText().trim();
-		 
-		 //check the input is has a value
-		 if(searchString.length() > 0) {
-			 
-			 //convert user input String searchString to integer
-			 int competitorNumber = Integer.parseInt(searchString);
-			 Competitor c = compList.findBycompetitorNumber2(competitorNumber);
-			 
-			 //if the competitor exists then return their details
-			 if (c!= null ) {
-				 compNumberField.setText(String.valueOf(c.getcompetitorNumber()));
-				 nameField.setText(c.getName().getFullName());
-				 locationField.setText(c.getcompetitorLocation());
-				 levelField.setText(c.getcompetitorLevel());
-				 //loop through scores field and scores array to display all scores
-				 for (int i = 0; i < 5; i++) {
-					 scoresField[i].setText("");
-					 }
-					 int [] scores = c.getScoreArray();
-					 for (int i = 0; i < scores.length; i++) {
-						 scoresField[i].setText(String.valueOf(scores[i]));
-					  }
-				 overallScore.setText(String.valueOf(c.getOverallScore()));
-				 update.setEnabled(true);
-			 }
-			 else {
-				compNumberField.setText("not found");
-			 	clear();
-			 }
-		 }
-			 else {
-				compNumberField.setText("no competitor entered");
-		 		clear();
-			 }
-		 }
-	 
-	  //updates scores
-	  private void update() {
-		  String searchString = searchField.getText().trim();
-		  int competitorNumber = Integer.parseInt(searchString);
-		  Competitor c = compList.findBycompetitorNumber2(competitorNumber);
-		  String newScoresStr[] = new String[5];
-		  for (int i = 0; i < 5; i++) {
-			  newScoresStr[i]=scoresField[i].getText();
-		  }
-		  int newScores[] = new int[5];
-		  	for(int i = 0; i < 5; i++ ) {
-		  		newScores[i] = Integer.parseInt(newScoresStr[i]);
-		  	}
-		  c.setcompetitorScores(newScores);
-		  overallScore.setText(String.valueOf(c.getOverallScore()));
-	  }
-	  
-	  private void clear() {
-		  nameField.setText("");
-		  locationField.setText("");
-		  levelField.setText("");
-		  for (int i = 0; i < 5; i++) {
-			  scoresField[i].setText("");
-		  }
-		  update.setEnabled(false);
-		  }
+
+	}
+
+	// THIS MIGHT NEED TO CHANGE DEPENDING ON HOW THE COMPLIST WILL
+	// RETURN THE LIST OF COMPETITORS FOR THE COMPETITIONS
+
+	// identify which option from the combo box was chosen and display
+	// the corresponding details for the selection
+	private void search() {
+		String competitionSelected = (String) competitionList.getSelectedItem();
+
+		// THIS WORKS - RETURNS LIST OF ALL COMPETITORS
+		if (competitionSelected == "All competitors") {
+			resultList.setText((reportHeadings()) + (compList.getCompetitorList()));
+		}
+
+		else if (competitionSelected == "Dancing") {
+
+			CompetitorList dancerList = new CompetitorList();
+
+			for (Competitor c : compList.getcompetitorList()) {
+				if (c instanceof Dancer) {
+					dancerList.add(c);
+				}
+
+			}
+
+			resultList.setText((reportHeadings()) + (dancerList.getCompetitorList()));
+		}
+
+		else if (competitionSelected == "Gaming") {
+
+			CompetitorList gamerList = new CompetitorList();
+
+			for (Competitor c : compList.getcompetitorList()) {
+				if (c instanceof Gamer) {
+					gamerList.add(c);
+				}
+
+			}
+
+			resultList.setText((reportHeadings()) + (gamerList.getCompetitorList()));
+		} else if (competitionSelected == "Knitting") {
+
+			CompetitorList knitterList = new CompetitorList();
+
+			for (Competitor c : compList.getcompetitorList()) {
+				if (c instanceof Knitter) {
+					knitterList.add(c);
+				}
+
+			}
+
+			resultList.setText((reportHeadings()) + (knitterList.getCompetitorList()));
+
+		}
+
+	}
+
+
+
+	// THE FOLLOWING TWO METHODS WILL ADDITIONAL ELSE STATEMENTS
+	// DEPENDING ON THE COMPETITION SELECTED
+	// ONCE THE COMPLIST METHODS ARE DONE
+
+	// identify which option from the combo box was chosen and display
+	// the corresponding details for the selection in name order
+	public void sortByName() {
+		String competitionSelected = (String) competitionList.getSelectedItem();
+		// THIS WORKS - RETURNS LIST OF ALL COMPETITORS IN ID ORDER
+		if (competitionSelected == "All competitors") {
+			resultList.setText((reportHeadings()) + (compList.listByName()));
+		} else if (competitionSelected == "Dancing") {
+
+			CompetitorList dancerList = new CompetitorList();
+
+			for (Competitor c : compList.getcompetitorList()) {
+				if (c instanceof Dancer) {
+					dancerList.add(c);
+				}
+
+			}
+
+			resultList.setText((reportHeadings()) + (dancerList.listByName()));
+		} else if (competitionSelected == "Gaming") {
+
+			CompetitorList gamerList = new CompetitorList();
+
+			for (Competitor c : compList.getcompetitorList()) {
+				if (c instanceof Gamer) {
+					gamerList.add(c);
+				}
+
+			}
+
+			resultList.setText((reportHeadings()) + (gamerList.listByName()));
+		} else if (competitionSelected == "Knitting") {
+
+			CompetitorList knitterList = new CompetitorList();
+
+			for (Competitor c : compList.getcompetitorList()) {
+				if (c instanceof Knitter) {
+					knitterList.add(c);
+				}
+
+			}
+
+			resultList.setText((reportHeadings()) + (knitterList.listByName()));
+		}
+
+		else
+			resultList.setText("not found");
+	}
+
+	// identify which option from the combo box was chosen and display
+	// the corresponding details for the selection in ID order
+	public void sortByID() {
+		String competitionSelected = (String) competitionList.getSelectedItem();
+		// THIS WORKS - RETURNS LIST OF ALL COMPETITORS IN NAME ORDER
+		if (competitionSelected == "All competitors") {
+			resultList.setText((reportHeadings()) + (compList.listByID()));
+		} else if (competitionSelected == "Dancing") {
+
+			CompetitorList dancerList = new CompetitorList();
+
+			for (Competitor c : compList.getcompetitorList()) {
+				if (c instanceof Dancer) {
+					dancerList.add(c);
+				}
+
+			}
+
+			resultList.setText((reportHeadings()) + (dancerList.listByID()));
+		} else if (competitionSelected == "Gaming") {
+
+			CompetitorList gamerList = new CompetitorList();
+
+			for (Competitor c : compList.getcompetitorList()) {
+				if (c instanceof Gamer) {
+					gamerList.add(c);
+				}
+
+			}
+
+			resultList.setText((reportHeadings()) + (gamerList.listByID()));
+		} else if (competitionSelected == "Knitting") {
+
+			CompetitorList knitterList = new CompetitorList();
+
+			for (Competitor c : compList.getcompetitorList()) {
+				if (c instanceof Knitter) {
+					knitterList.add(c);
+				}
+			}
+			resultList.setText((reportHeadings()) + (knitterList.listByID()));
+			
+		} else
+			resultList.setText("not found");
+	}
 	
+	private String reportHeadings() {
+		String report = "";
+		report += (String.format("%-20s", "Competition"));
+		report += (String.format("%-6s", "CN"));
+		report += (String.format("%-40s", "Name"));
+		report += (String.format("%-20s", "Experience"));
+		report += (String.format("%-20s", "Location"));
+		report += (String.format("%-6s", "Age"));
+		report += (String.format("%-20s", "Scores"));
+		report += (String.format("%-15s", "Overall Score\n"));
+		report += ("----------------------------------------------------------------------------------------------------------------------------------------------------\n");
+		return report;
+	}
 }
